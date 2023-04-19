e2dfee5dba643a1212b6d2ed9d0c8b34ec744ace
Marla Gualdron <ma.gualdron@gmail.com>
2023-04-19 19:53:43 -0300

    adding route show for movies

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -5,8 +5,17 @@ class MovieController < ApplicationController
     render json: movies, status: :ok
   end
 
+  def show
+    render json: movie, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'ID not found' }, status: :not_found
+  end
   private
 
+  def movie
+    Movie.find(params.require([:id]))
+  end
+  
   def movies
     Movie.page(params[:page]).per(params[:page_size])
   end
