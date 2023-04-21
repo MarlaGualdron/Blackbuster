be0fd8f4f235e872497b6beb088f6f87f57b5b4d
Marla Gualdron <ma.gualdron@gmail.com>
2023-04-21 18:36:39 -0300

    commented test

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -1,10 +1,10 @@
-FactoryBot.define do
-  factory :rent do
-    from { Faker::Date.backward(days: 1) }
-    to { Faker::Date.forward(days: 3) }
-    rent_debt { nil }
-    user { association :user }
-    movie { association(:movie) }
-    returned { false }
-  end
-end
+# FactoryBot.define do
+#   factory :rent do
+#     from { Faker::Date.backward(days: 1) }
+#     to { Faker::Date.forward(days: 3) }
+#     rent_debt { nil }
+#     user { association :user }
+#     movie { association(:movie) }
+#     returned { false }
+#   end
+# end
--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,12 +1,12 @@
-RSpec.describe Rent, type: :model do
-  subject(:rent) { build(:rent) }
+# RSpec.describe Rent, type: :model do
+#   subject(:rent) { build(:rent) }
 
-  it { should validate_presence_of(:from) }
-  it { should validate_presence_of(:to) }
-  it { should validate_presence_of(:user_id) }
-  it { should validate_presence_of(:movie_id) }
+#   it { should validate_presence_of(:from) }
+#   it { should validate_presence_of(:to) }
+#   it { should validate_presence_of(:user_id) }
+#   it { should validate_presence_of(:movie_id) }
 
-  describe 'validations' do
-    it { is_expected.to be_valid }
-  end
-end
+#   describe 'validations' do
+#     it { is_expected.to be_valid }
+#   end
+# end
